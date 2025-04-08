463bd1a60c8d7b00abf9834646b3d2ce8216569e
Rodrigo Barbosa <34305033+rodrigofcb@users.noreply.github.com>
2025-04-07 21:26:55 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'screen-sm': '640px',
+        'screen-md': '768px',
+      },
+      fontSize: {
+        base: ['1.125rem', '1.75rem'], // 18px com linha confortável
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
+  darkMode: 'media', // alterna automaticamente com base no SO do usuário
   plugins: [],
 }
